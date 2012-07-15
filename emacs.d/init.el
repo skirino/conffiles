e57be9a677c72256446deed7249da152739d66cd
Shunsuke Kirino <Shunsuke.Kirino@access-company.com>
2012-07-15 17:38:05 +0900

    Change default font on Linux

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -751,7 +751,7 @@
      ;; If you edit it by hand, you could mess it up, so be careful.
      ;; Your init file should contain only one such instance.
      ;; If there is more than one, they won't work right.
-     '(default ((t (:inherit nil :stipple nil :background nil :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "unknown" :family "Droid Sans Mono")))))
+     '(default ((t (:inherit nil :stipple nil :background nil :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "unknown" :family "Dejavu Sans Mono")))))
   )
 )
 
