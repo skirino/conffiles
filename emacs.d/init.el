8fd07e98b98f26e8bc0b90ed5b90ecd25700a7cc
Shunsuke Kirino <Shunsuke.Kirino@access-company.com>
2013-04-22 04:29:05 +0900

    Add multiple-cursors.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -286,6 +286,17 @@ The list is written to FILENAME, or `save-packages-file' by default."
 
 
 
+;;;;;;;;;;;;;;;;;;;;;;;; multiple-cursors
+(require 'multiple-cursors)
+(global-set-key (kbd "C-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C-)") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-(") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-(") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-c C-)") 'mc/mark-all-like-this)
+;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;; undo & redo
 (require 'undo-tree)
 (global-undo-tree-mode)
