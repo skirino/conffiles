7f5663e9fa565292194d8f417c8d37305d337365
Shunsuke Kirino <Shunsuke.Kirino@access-company.com>
2013-07-16 15:18:18 +0900

    Move location of setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,6 @@ echo "
 . $HOME/code/conffiles/zsh/zshrc
 " > ~/.zshrc
 
-ln -s code/conffiles/emacs.d ~/.emacs.d
-
+ln -s code/conffiles/emacs.d     ~/.emacs.d
 ln -s code/conffiles/keysnail.js ~/.keysnail.js
 
