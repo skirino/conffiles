7ffbb7e30c1ccf2044f807b74c984bd8974ed941
Shunsuke Kirino <Shunsuke.Kirino@access-company.com>
2013-12-09 11:22:20 +0900

    Remove howm

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -593,19 +593,6 @@ The list is written to FILENAME, or `save-packages-file' by default."
 
 
 
-;;;;;;;;;;;;;;;;;;;;;;;; howm-mode
-(setq howm-sample-directory (expand-file-name "~/docs/howm/"))
-(setq howm-directory        howm-sample-directory)
-(setq howm-keyword-file     (expand-file-name ".howm-keys"    howm-sample-directory))
-(setq howm-history-file     (expand-file-name ".howm-history" howm-sample-directory))
-(setq howm-menu-lang        'ja)
-(setq howm-history-limit    nil)  ;; Don't erase my ~/.howm-history.
-
-(require 'howm)
-;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-
 ;;;;;;;;;;;;;;;;;;;;;;;; Git (egg.el)
 (require 'egg)
 
