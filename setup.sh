574c823b3bfe25bb16bd3794edb89fa4138d06f0
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2014-04-24 09:33:48 +0900

    Add xmonad settings

--- setup.sh
+++ setup.sh
@@ -8,6 +8,8 @@ echo "
 . $HOME/code/conffiles/zsh/zshrc
 " > ~/.zshrc
 
-ln -s code/conffiles/emacs.d     ~/.emacs.d
-ln -s code/conffiles/keysnail.js ~/.keysnail.js
-
+ln -s ~/code/conffiles/emacs.d     ~/.emacs.d
+ln -s ~/code/conffiles/keysnail.js ~/.keysnail.js
+mkdir -p ~/.xmonad
+ln -s ~/code/conffiles/xmonad/xmonad.hs ~/.xmonad/xmonad.hs
+ln -s ~/code/conffiles/xmonad/xmobarrc ~/.xmobarrc
