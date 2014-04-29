d3f97e8df2e749c0a79a5f7b30698c6e702db13b
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2014-04-30 05:06:53 +0900

    Add percol

--- setup.sh
+++ setup.sh
@@ -13,3 +13,5 @@ ln -s ~/code/conffiles/keysnail.js ~/.keysnail.js
 mkdir -p ~/.xmonad
 ln -s ~/code/conffiles/xmonad/xmonad.hs ~/.xmonad/xmonad.hs
 ln -s ~/code/conffiles/xmonad/xmobarrc ~/.xmobarrc
+mkdir -p ~/.percol.d
+ln -s ~/code/conffiles/percol/rc.py ~/.percol.d/rc.py
