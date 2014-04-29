e6c5644d8796a274a718a8d4be14d2495c2425f6
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2014-04-30 04:01:52 +0900

    Modify keybinds

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -7,55 +7,43 @@
 -- Normally, you'd only override those defaults you care about.
 --
 
+import Data.Monoid
+import System.Exit
+import System.IO
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
-import Data.Monoid
-import System.Exit
-import System.IO
-
+import XMonad.Util.WorkspaceCompare
+import XMonad.Actions.CycleWS
+import XMonad.Layout.Named
+import XMonad.Layout.MultiToggle
+import XMonad.Layout.MultiToggle.Instances
+import XMonad.Config.Desktop (desktopLayoutModifiers)
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
-    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)                 -- launch a terminal
-    , ((modm              , xK_p     ), spawn "dmenu_run")                            -- launch dmenu
-    , ((modm .|. shiftMask, xK_c     ), kill)                                         -- close focused window
-    , ((modm              , xK_space ), sendMessage NextLayout)                       -- Rotate through the available layout algorithms
-    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)           -- Reset the layouts on the current workspace to default
-    , ((modm              , xK_n     ), refresh)                                      -- Resize viewed windows to the correct size
-    , ((modm              , xK_Tab   ), windows W.focusDown)                          -- Move focus to the next window
-    , ((modm              , xK_j     ), windows W.focusDown)                          -- Move focus to the next window
-    , ((modm              , xK_k     ), windows W.focusUp  )                          -- Move focus to the previous window
-    , ((modm              , xK_m     ), windows W.focusMaster)                        -- Move focus to the master window
-    , ((modm              , xK_Return), windows W.swapMaster )                        -- Swap the focused window and the master window
-    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown)                           -- Swap the focused window with the next window
-    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp  )                           -- Swap the focused window with the previous window
-    , ((modm              , xK_h     ), sendMessage Shrink)                           -- Shrink the master area
-    , ((modm              , xK_l     ), sendMessage Expand)                           -- Expand the master area
-    , ((modm              , xK_t     ), withFocused $ windows . W.sink)               -- Push window back into tiling
-    , ((modm              , xK_comma ), sendMessage (IncMasterN 1))                   -- Increment the number of windows in the master area
-    , ((modm              , xK_period), sendMessage (IncMasterN (-1)))                -- Deincrement the number of windows in the master area
-    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))                    -- Quit xmonad
-    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart") -- Restart xmonad
+    [ ((modm              , xK_Return), spawn "dmenu_run -fn 'Monospace-18'") -- launch dmenu
+    , ((modm .|. shiftMask, xK_t     ), spawn $ XMonad.terminal conf)
+    , ((modm .|. shiftMask, xK_e     ), spawn "emacs")
+    , ((modm .|. shiftMask, xK_f     ), spawn "firefox")
+    , ((modm .|. shiftMask, xK_s     ), spawn "seta")
+    , ((modm .|. shiftMask, xK_c     ), kill)                                 -- close focused window
+    , ((modm              , xK_j     ), windows W.focusDown)                  -- Move focus to the next window
+    , ((modm              , xK_k     ), windows W.focusUp  )                  -- Move focus to the previous window
+    , ((modm              , xK_period), moveTo Next NonEmptyWS)
+    , ((modm              , xK_comma ), moveTo Prev NonEmptyWS)
     ]
-    ++
 
     -- mod-[1..9], Switch to workspace N
     -- mod-shift-[1..9], Move client to workspace N
+    ++
     [((m .|. modm, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
-    ++
-
-    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
-    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
-    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 ------------------------------------------------------------------------
 -- Mouse bindings: default actions bound to mouse events
