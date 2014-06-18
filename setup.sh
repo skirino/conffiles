be3a164f815121bad031bd66c2bf702c907a7772
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2014-06-18 15:09:41 +0900

    Add vimrc

--- setup.sh
+++ setup.sh
@@ -8,8 +8,9 @@ echo "
 . $HOME/code/conffiles/zsh/zshrc
 " > ~/.zshrc
 
-ln -s ~/code/conffiles/emacs.d     ~/.emacs.d
+ln -s ~/code/conffiles/emacs.d ~/.emacs.d
 ln -s ~/code/conffiles/keysnail.js ~/.keysnail.js
+ln -s ~/code/conffiles/vimrc ~/.vimrc
 mkdir -p ~/.xmonad
 ln -s ~/code/conffiles/xmonad/xmonad.hs ~/.xmonad/xmonad.hs
 ln -s ~/code/conffiles/xmonad/xmobarrc ~/.xmobarrc
---
+++ vimrc
@@ -0,0 +1,4 @@
+syntax on
+
+" Disable C-@ (and C-S-Space) since it is used to disable Japanese input method
+autocmd VimEnter * imap <C-@> <nop>
