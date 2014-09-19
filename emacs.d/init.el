55870174563d69d0da7d44a478c83f0bc575cbc0
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2014-09-20 00:59:18 +0900

    Replace git-gutter-fringe with git-gutter-fringe+

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -575,20 +575,15 @@ The list is written to FILENAME, or `save-packages-file' by default."
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;; Git (egg.el)
-;; git-gutter & git-gutter-fringe
-(require 'git-gutter-fringe)
-(global-git-gutter-mode t)
-(global-set-key (kbd "C-c n"  ) 'git-gutter:next-hunk)
-(global-set-key (kbd "C-c C-n") 'git-gutter:next-hunk)
-(global-set-key (kbd "C-c p"  ) 'git-gutter:previous-hunk)
-(global-set-key (kbd "C-c C-p") 'git-gutter:previous-hunk)
-(defun my-git-gutter-stage-hunk ()
-  (interactive)
-  (git-gutter:awhen (git-gutter:search-here-diffinfo git-gutter:diffinfos)
-    (git-gutter:do-stage-hunk it)
-    (git-gutter)))
-(global-set-key (kbd "C-c s"  ) 'my-git-gutter-stage-hunk)
-(global-set-key (kbd "C-c C-s") 'my-git-gutter-stage-hunk)
+;; git-gutter+ & git-gutter-fringe+
+(require 'git-gutter-fringe+)
+(global-git-gutter+-mode t)
+(global-set-key (kbd "C-c n"  ) 'git-gutter+-next-hunk)
+(global-set-key (kbd "C-c C-n") 'git-gutter+-next-hunk)
+(global-set-key (kbd "C-c p"  ) 'git-gutter+-previous-hunk)
+(global-set-key (kbd "C-c C-p") 'git-gutter+-previous-hunk)
+(global-set-key (kbd "C-c s"  ) 'git-gutter+-stage-hunks)
+(global-set-key (kbd "C-c C-s") 'git-gutter+-stage-hunks)
 ;;;;;;;;;;;;;;;;;;;;;;;;
 
 
