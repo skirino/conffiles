4aadfd1c970b418b5f75198b52af57a4947d7f3e
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2017-11-27 15:00:52 +0900

    Update setup of gtk keybindings

--- setup.sh
+++ setup.sh
@@ -9,10 +9,14 @@ echo "
 " > ~/.zshrc
 
 ln -s ~/code/conffiles/emacs.d ~/.emacs.d
-ln -s ~/code/conffiles/keysnail.js ~/.keysnail.js
 ln -s ~/code/conffiles/vimrc ~/.vimrc
 mkdir -p ~/.xmonad
 ln -s ~/code/conffiles/xmonad/xmonad.hs ~/.xmonad/xmonad.hs
 ln -s ~/code/conffiles/xmonad/xmobarrc ~/.xmobarrc
+ln -s ~/code/conffiles/xbindkeysrc ~/.xbindkeysrc
 mkdir -p ~/.percol.d
 ln -s ~/code/conffiles/percol/rc.py ~/.percol.d/rc.py
+mkdir -p ~/.config/gtk-3.0
+ln -s ~/code/conffiles/gtk/settings.ini ~/.config/gtk-3.0/settings.ini
+mkdir -p ~/.themes/Emacs/gtk-3.0
+ln -s ~/code/conffiles/gtk/gtk-keys.css ~/.themes/Emacs/gtk-3.0/gtk-keys.css
