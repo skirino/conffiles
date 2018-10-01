ce2fd6df990d2f15086bbf9390cd69c37c574cfa
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2018-10-01 21:25:44 +0900

    Remove marmalade as default package registry

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -18,11 +18,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;; packages
 (require 'package)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(package-initialize)
-
 (require 'async-bytecomp)
-
 (require 'save-packages)
 ;;;;;;;;;;;;;;;;;;;;;;;;
 
