024babd85ea9d8b16f4cb5a42f2e3f80f9d38663
Shunsuke Kirino <skirino@flywheel.jp>
2022-11-25 06:16:47 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -euo pipefail -o posix
+
+confdir=$(dirname $(realpath "$0"))
+
+(
+  cd "${confdir}"
+  git submodule update --init
+)
 
 echo "
-. $HOME/code/conffiles/zsh/zprofile
-" >> ~/.profile
+. ${confdir}/zsh/zshrc
+" >> ~/.zshrc
 
-echo "
-. $HOME/code/conffiles/zsh/zshrc
-" > ~/.zshrc
-
-ln -s ~/code/conffiles/emacs.d ~/.emacs.d
-ln -s ~/code/conffiles/vimrc ~/.vimrc
-mkdir -p ~/.xmonad
-ln -s ~/code/conffiles/xmonad/xmonad.hs ~/.xmonad/xmonad.hs
-ln -s ~/code/conffiles/xmonad/xmobarrc ~/.xmobarrc
-ln -s ~/code/conffiles/xbindkeysrc ~/.xbindkeysrc
-mkdir -p ~/.percol.d
-ln -s ~/code/conffiles/percol/rc.py ~/.percol.d/rc.py
-mkdir -p ~/.config/gtk-3.0
-ln -s ~/code/conffiles/gtk/settings.ini ~/.config/gtk-3.0/settings.ini
-mkdir -p ~/.themes/Emacs/gtk-3.0
-ln -s ~/code/conffiles/gtk/gtk-keys.css ~/.themes/Emacs/gtk-3.0/gtk-keys.css
+ln -s "${confdir}/emacs.d" ~/.emacs.d
+emacs -nw --script ~/.emacs.d/init.el
