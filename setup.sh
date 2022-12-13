ea7cb5c5783136acb878ff0364cc5bdc3195adfd
Shunsuke Kirino <skirino@flywheel.jp>
2022-12-14 04:59:17 +0900

    Add setup for direnv_rust

--- setup.sh
+++ setup.sh
@@ -12,5 +12,9 @@ echo "
 . ${confdir}/zsh/zshrc
 " >> ~/.zshrc
 
+mkdir -p ~/bin
+curl -L https://github.com/skirino/direnv_rust/releases/download/0.1.0/direnv_rust_linux_amd64 --output ~/bin/direnv_rust
+chmod +x ~/bin/direnv_rust
+
 ln -s "${confdir}/emacs.d" ~/.emacs.d
 emacs -nw --script ~/.emacs.d/init.el
